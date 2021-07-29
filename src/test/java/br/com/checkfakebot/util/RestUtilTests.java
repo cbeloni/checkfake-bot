4c8fdf5dd8102c8a1555b4a4a2de4ce1938a56d7
Cauê Beloni <cauebeloni@Beloni-A60-MUV>
2021-07-29 19:24:34 -0300

    Inclusão salvar imagem meme resttemplate

--- src/test/java/br/com/checkfakebot/util/RestUtilTests.java
+++ src/test/java/br/com/checkfakebot/util/RestUtilTests.java
@@ -3,21 +3,31 @@ package br.com.checkfakebot.util;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.checkfakebot.dto.MemeDTO;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "local")
 public class RestUtilTests {
 	
 	@Autowired
 	private RestUtils restUtils;
 	
-	@Test
+	@Autowired
+    ResourceLoader resourceLoader;
+	
+	//@Test
 	void chamadaRemoteService() {
 		MemeDTO memeDTO = restUtils.getMemePorId("1");
 		
@@ -26,10 +36,22 @@ public class RestUtilTests {
 	}
 	
 	@Test
-	void obtemArquivo() {
-		ClassPathResource res = new ClassPathResource("teste.txt");    
-		File file = new File(res.getPath());
+	void salvaImagem() throws IOException {
+		byte[] arquivo = obtemArquivo();
+		
+		MemeDTO memeDTO = restUtils.salvarMeme(arquivo, "pensando");
 		
 		assertTrue(true);
+	}
+	
+	byte[] obtemArquivo() throws IOException {
+		
+	    Resource resource = resourceLoader.getResource("classpath:pensando.png");
+
+	    InputStream input = resource.getInputStream();
+	    
+	    byte[] fileBytes = input.readAllBytes(); 
+		
+		return fileBytes;
 	}
 }
