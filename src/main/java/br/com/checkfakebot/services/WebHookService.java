f6b002a6a81aee909ff0bbfb79f3c2d6a67d4855
cauebeloni <cauebeloni@Beloni-A60-MUV>
2021-08-03 17:57:15 -0300

    Adicionado hash

--- src/main/java/br/com/checkfakebot/services/WebHookService.java
+++ src/main/java/br/com/checkfakebot/services/WebHookService.java
@@ -1,18 +1,28 @@
 package br.com.checkfakebot.services;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.com.checkfakebot.config.OauthProperties;
+import br.com.checkfakebot.util.HMAC;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
 @Service
 @Slf4j
 public class WebHookService {
+	
+	@Autowired
+	private OauthProperties oauthProperties;
 	
 	public String listenerWebhook(Object body) throws JsonGenerationException, JsonMappingException, IOException {
 		
@@ -22,9 +32,9 @@ public class WebHookService {
 		return json;
 	}
 	
-	public void getWebHook() {
-		log.info("Feito Get no webhook " );
-	}
-	
+	public String getWebHook(String crcToken) throws UnsupportedEncodingException {
+		log.info("Feito Get no webhook " + crcToken);
+		return HMAC.calcHmacSha256(oauthProperties.getConsumersecret().getBytes("UTF-8"), crcToken.getBytes("UTF-8"));		
+	}	
 
 }
