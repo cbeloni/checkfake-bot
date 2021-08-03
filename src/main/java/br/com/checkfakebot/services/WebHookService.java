d98aded418f550ecc22fa9d0f0a4c005180a3566
cauebeloni <cauebeloni@Beloni-A60-MUV>
2021-08-03 16:02:10 -0300

    endpoint body genérico

---
+++ src/main/java/br/com/checkfakebot/services/WebHookService.java
@@ -0,0 +1,26 @@
+package br.com.checkfakebot.services;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class WebHookService {
+	
+	public void listenerWebhook(Object body) throws JsonGenerationException, JsonMappingException, IOException {
+		
+		String json = new ObjectMapper().writeValueAsString(body);
+
+		log.info("Objeto recebido: " + json);
+	}
+	
+
+}
